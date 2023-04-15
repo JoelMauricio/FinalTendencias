4d188bc318e24d17730e43cd95727476e62eb62a
Joel <1105039@est.intec.edu.do>
2023-04-15 00:13:59 -0400

    Added Message component and scroll funcitonality

--- class-chat/components/ChatsList.js
+++ class-chat/components/ChatsList.js
@@ -6,59 +6,16 @@ export default function ChatsList() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        fetchChats();
-    }, []);
-
-    async function fetchChats() {
-        try {
-            setLoading(true);
-            const user = supabase.auth.user();
-            const { data: chats, error } = await supabase
-                .from('chats')
-                .select(`
-                    id,
-                    name,
-                    users (
-                        id,
-                        email,
-                        name
-                    )
-                `)
-                .eq('users.id', user.id);
-
-            if (error) throw error;
-            setChats(chats);
-        } catch (error) {
-            setError(error.message);
-        } finally {
-            setLoading(false);
-        }
-    }
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
-    if (error) {
-        return <div>{error}</div>;
-    }
+    // useEffect(() => {
+    //     fetchChats();
+    // }, []);
 
     return (
-        <div className="flex flex-col items-center justify-center min-w-full min-h-[40rem] h-full bg-gray-100">
-            <h1 className="text-3xl font-bold">Chats</h1>
+        <div className="flex flex-col items-center justify-center min-w-full min-h-[40rem] h-full">
             <div className="flex flex-col items-center justify-center w-full h-full gap-4">
                 {chats.map((chat) => (
                     <div key={chat.id} className="flex flex-col items-center justify-center w-full h-full gap-4">
-                        <h1 className="text-2xl font-bold">{chat.name}</h1>
-                        <div className="flex flex-col items-center justify-center w-full h-full gap-4">
-                            {chat.users.map((user) => (
-                                <div key={user.id} className="flex flex-col items-center justify-center w-full h-full gap-4">
-                                    <h1 className="text-xl font-bold">{user.name}</h1>
-                                    <h1 className="text-lg font-bold">{user.email}</h1>
-                                </div>
-                            ))}
-                        </div>
+                        <div className="rounded-full h-[3rem] w-[3rem] bg-red-400"></div>
                     </div>
                 ))}
             </div>
