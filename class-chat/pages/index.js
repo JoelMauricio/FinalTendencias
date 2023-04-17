cac9fa35f3f61a04bf722f36a4f0a7327821e34e
Joel <1105039@est.intec.edu.do>
2023-04-17 10:55:39 -0400

    Added Prettier for lint fix and PORT customization

--- class-chat/pages/index.js
+++ class-chat/pages/index.js
@@ -1,28 +1,26 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { supabase } from './../lib/supabaseClient';
-import Chat from '@/components/Chat';
-import Nav from '@/components/Nav';
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { supabase } from "./../lib/supabaseClient";
+import Chat from "@/components/Chat";
+import Nav from "@/components/Nav";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
       <div className="flex flex-col items-center justify-center w-full h-full gap-4">
         <Nav />
-        <div className=' h-screen max-h-[1000px] w-full'>
+        <div className=" h-screen max-h-[1000px] w-full">
           <Chat />
         </div>
       </div>
     </main>
-  )
+  );
 }
 
 export async function getServerSideProps() {
   return {
-    props: {
-    },
-  }
+    props: {},
+  };
 }
-
