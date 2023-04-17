ae3ab8e546b220d3ce5e1c0a5bcacf676d51ee15
Joel <1105039@est.intec.edu.do>
2023-04-17 17:02:46 -0400

    fixed navbar

--- class-chat/components/Nav.js
+++ class-chat/components/Nav.js
@@ -19,7 +19,7 @@ export default function Nav() {
     <nav className="flex w-full h-fit justify-between px-8">
       <h1 className="text-[1.5rem] font-semibold">ClassChat</h1>
       <div className="flex gap-4">
-        {!session ? (<button className="text-[1.25rem]" onClick={handleLogout}>Salir</button>) : (null)}
+        {session ? (<button className="text-[1.25rem]" onClick={handleLogout}>Salir</button>) : (null)}
       </div>
     </nav>
   );
