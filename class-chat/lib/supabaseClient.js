ff2ec8b796e89e2609d46b0705dbab0080fd224e
Joel <1105039@est.intec.edu.do>
2023-04-16 23:54:45 -0400

    added scrollbar style and .env.local

--- class-chat/lib/supabaseClient.js
+++ class-chat/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-    'https://ofouyycoclzmipiqgfpv.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9mb3V5eWNvY2x6bWlwaXFnZnB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE0OTEwOTEsImV4cCI6MTk5NzA2NzA5MX0.garhuw4-PXeuNv13kzdiB5olKJ1K76wTXE8jBMIcf-w',
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 )
